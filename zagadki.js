1a2fbfa059750064334b3597e678480df0d10e0d
kajetankow <66178267+kajetankow@users.noreply.github.com>
2021-05-15 19:03:23 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,55 @@
+function z1()
+{
+      document.getElementById("z1").innerHTML="Ktoś popchnął akwarium, które się rozbiło a zginęła rybka.";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Kliknij przycisk aby zobaczyć odpowiedź!";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="Rzuciłeś piłkę pionowo do góry.";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Kliknij przycisk aby zobaczyć odpowiedź!";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Są to numery apartamentów. Jeden kosztuje 100zł.";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Kliknij przycisk aby zobaczyć odpowiedź!";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="Literą M.";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Kliknij przycisk aby zobaczyć odpowiedź!";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Obietnicę.";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Kliknij przycisk aby zobaczyć odpowiedź!";
+    }
